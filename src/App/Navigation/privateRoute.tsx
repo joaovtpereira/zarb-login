e736e44e2cb758f78da39a692f3eb8dda1ada494
JoaoVitorPereiraUFU <joaovitorpessoal2@gmail.com>
2020-09-02 11:16:17 -0300

    fix: refatoracao de codigo

--- src/App/Navigation/privateRoute.tsx
+++ src/App/Navigation/privateRoute.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 import { Route, Redirect, RouteProps } from 'react-router'
 
-import Dashboard from '../Containers/Dashboard';
-
 const PrivateRoute  = React.forwardRef<HTMLAnchorElement, RouteProps>((props, ref)  => {
     const isLogged = !!localStorage.getItem('app-token');
     return isLogged ? <Route {...props}/> : <Redirect to="/login"/>
--- src/App/Services/api.ts
+++ src/App/Services/api.ts
@@ -4,14 +4,4 @@ const api = axios.create({
   baseURL: 'http://localhost:3333/',
 });
 
-// api.interceptors.request.use((config) => {
-//   const newConfig = config;
-//   const token = localStorage.getItem('@Auth/access_token');
-//   if (!token && config.url !== '/session') {
-//     throw new axios.Cancel('no token');
-//   }
-//   newConfig.headers.Authorization = token;
-//   return newConfig;
-// });
-
 export default api;
